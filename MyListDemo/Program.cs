bf1af5b2d7ca1ef0962717274379d7fd3fa2085b
tayfun mutlu <mutlutayfun1@gmail.com>
2022-04-18 12:45:53 +0300

    18.04

---
+++ MyListDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using MyListDemo;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("örnek");
+            Console.WriteLine(list.Count);
+
+            MyListDemo<string> demo = new MyListDemo<string>();
+            demo.Add("deneme1");
+            Console.WriteLine(demo.Count);
+
+            
+        }
+    }
+
+    
+
+
+}
